1a05c3cbd7f44472d9fa1ae7d4fa38de978626f4
stephmen <stephmen@gmail.com>
2019-06-19 18:29:44 -0400

    Adding Home Page

---
+++ frontend/components/HomePage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+
+
+class HomePage extends Component {
+  render() {
+    return (
+
+      <div>
+        <p>Bienvenue sur cette page d'acceuil</p>
+      </div>
+
+    );
+  }
+}
+
+export default HomePage;
---
+++ frontend/pages/homepage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+
+
+class homepage extends Component {
+  render() {
+    return (
+
+      <div>
+        <p>Bienvenue sur cette page d'acceuil</p>
+      </div>
+
+    );
+  }
+}
+
+export default homepage;
